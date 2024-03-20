f5b0bf4e3fa8c62c97d0220fe07a2fa1c3159b24
Edwi Feitoza <edwi.slacker@gmail.com>
2024-03-19 21:36:05 -0300

    Adição de client para health check

--- src/main/java/br/com/sicredi/productstests/App.java
+++ src/main/java/br/com/sicredi/productstests/App.java
@@ -1,9 +1,13 @@
 package br.com.sicredi.productstests;
 
+import br.com.sicredi.productstests.healthcheck.HealthCheckVerifier;
+
+import java.io.IOException;
+
 public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) throws IOException {
+        var healthCheck = new HealthCheckVerifier();
+        healthCheck.verifyHealthCheck();
     }
 }
--- src/main/java/br/com/sicredi/productstests/healthcheck/HealthCheckVerifier.java
+++ src/main/java/br/com/sicredi/productstests/healthcheck/HealthCheckVerifier.java
@@ -1,5 +1,32 @@
 package br.com.sicredi.productstests.healthcheck;
 
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+import java.io.IOException;
+
 public class HealthCheckVerifier {
-    
+
+    private static final String ROOT_URL = "https://dummyjson.com";
+    private OkHttpClient client;
+
+    public HealthCheckVerifier() {
+        this.client = new OkHttpClient();
+    }
+
+    public void verifyHealthCheck() throws IOException{
+        var request = new Request.Builder()
+                .url(ROOT_URL + "/test")
+                .build();
+
+        try(var response = this.client.newCall(request).execute()){
+            if(!response.isSuccessful()) throw new IOException("Unexpected response code" + response);
+            Headers responseHeaders = response.headers();
+            for (Integer i = 0; i < responseHeaders.size(); i++) {
+                System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+            }
+            System.out.println(response.body().string());
+        }
+    }
 }
